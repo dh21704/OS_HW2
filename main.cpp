14b6b6a902c613920a178a561198e14fbc1a7882
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-02-18 18:20:35 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 25
+#define READ_END 0
+#define WRITE_END 1
+
+#define MIN_PID 100
+#define MAX_PID 1000
+
+#include <stdlib.h>
+#include <time.h>
+
+
+#include <vector>
+
+using namespace std;
+
+//creates and initializes the bitmap for PID management
+int allocate_map()
+{
+    bool *pid_map;
+    
+    srand(time(0));
+    pid_map = (bool*)malloc(sizeof(bool) * MAX_PID);
+    
+    return pid_map == NULL ? -1 : 1;
+    
+}
+
+int main(void)
+{
+    char write_msg[BUFFER_SIZE] = "Greetings";
+    char read_msg[BUFFER_SIZE];
+
+    /* create the pipe */
+    int fd[2];
+    pid_t pid;
+
+    /* error checking */
+    if (pipe(fd) == -1) {
+        fprintf(stderr, "Pipe failed");
+        return 1;
+    }
+
+    /* fork a child process */
+    pid = fork();
+
+//if the fork fails then the pid is negative 
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+
+    if (pid > 0) { /* parent process */
+        /* close the unused end of the pipe */
+        close(fd[READ_END]);
+
+        /* write to the pipe */
+        write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
+
+        /* close the write end of the pipe */
+        close(fd[WRITE_END]);
+    }
+    else { /* child process */
+        /* close the unused end of the pipe */
+        close(fd[WRITE_END]);
+
+        /* read from the pipe */
+        read(fd[READ_END], read_msg, BUFFER_SIZE);
+        printf("read %s", read_msg);
+
+        /* close the read end of the pipe */
+        close(fd[READ_END]);
+    }
+
+    return 0;
+}
